e6256b7ffb97f7bcdf9548a49a973f38baf084ad
beladibarnabas2 <barnabasbeladi@gmail.com>
2025-03-14 11:15:47 +0100

    droppoljal mar carti

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function hatja()
     let futasteljesitmeny = document.getElementById("futasteljesitmeny").value;
     let fogyasztas = document.getElementById("fogyasztas").value;
     let uzemanyag = document.cigany.uzemanyag.value
-
+    coKibocsatas = futasteljesitmeny * (fogyasztas/100)
     if (uzemanyag == "benzin")
     {
-        let coKibocsatas = futasteljesitmeny * (fogyasztas/100) * 2.31;
+        coKibocsatas * 2.31;
     }
     else if (uzemanyag == "dizel")
     {
-        let coKibocsatas = futasteljesitmeny * (fogyasztas/100) * 2.68;
+        coKibocsatas * 2.68;
     }
-
+    document.getElementById("co").innerHTML = ``
     
 }
